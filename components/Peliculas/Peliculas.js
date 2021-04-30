4f9a241d5305d2039394212a525d05ac75126a27
anderdavid <anderdavid86@gmail.com>
2021-04-30 11:59:14 -0500

    Se implementa filtro

--- components/Peliculas/Peliculas.js
+++ components/Peliculas/Peliculas.js
@@ -1,5 +1,6 @@
 import React,{ useEffect, useState } from 'react';
-import { StyleSheet, Text, View,FlatList } from 'react-native'
+import { StyleSheet, Text, View} from 'react-native'
+import { RadioButton } from 'react-native-paper';
 import Header from '../header';
 import TodoItem from '../todoItem';
 import List from '../list';
@@ -21,12 +22,43 @@ const Peliculas =(props)=>{
       
     }, []);
 
-    
+    const [checked, setChecked] = React.useState('');
 
     return (
         
         <View>
           <Header/>
+          <View>
+            <Text>Movies</Text>
+            <RadioButton
+            
+              value="first"
+              status={ checked === 'first' ? 'checked' : 'unchecked' }
+              onPress={() =>{
+                setChecked('first');
+                getPeliculas({
+                  s:'love',
+                  y:'2020',
+                  apikey:'5eec5adc',
+                  type:'movie'
+                });
+              } }
+            />
+            <Text>Series</Text>
+            <RadioButton
+              value="second"
+              status={ checked === 'second' ? 'checked' : 'unchecked' }
+              onPress={() => {
+                setChecked('second');
+                getPeliculas({
+                  s:'love',
+                  y:'2020',
+                  apikey:'5eec5adc',
+                  type:'series'
+                });
+              }}
+            />
+          </View>
           {peliculas&&(
             <List peliculas={peliculas.Search}/>
           )}
